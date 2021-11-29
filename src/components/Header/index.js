c998c144cb1a288988b1de2c09a98e91763ddac5
jorge <jrmaiworm@hotmail.com>
2021-11-29 11:43:58 -0300

    up

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,71 +1,39 @@
-import { Link } from "react-router-dom";
-import { MenuButton } from "../../styledComponents/BotaoMenu/styles";
-import { Menu } from "../../styledComponents/Menu/styles";
-import {Nav,DropdownItem,NavItem,NavLink,Dropdown,DropdownToggle,DropdownMenu}  from "reactstrap";
+import "../../styles/global";
 
 function Header () {
     
     return (
         <>
+<div class="menu" >
+              <nav  >
+                <ul>
 
-<div>
-  <Nav tabs>
-    <NavItem>
-      <NavLink
-        active
-        href="/"
-      >
-        Menu Principal
-      </NavLink>
-    </NavItem>
-    <Dropdown
-      nav
-      toggle={function noRefCheck(){
+                    <li ><a href="/">Home</a></li>
+                    <li><a href="../Prod_por_Categoria/esqueletoProdutosPorCategoria.html">Categorias</a>
+                    <ul>
+                      <li class="dropdown" onclick="/filmes">Filmes</li>
+                      <li class="dropdown" onclick="/series">Series </li>
+                      <li class="dropdown" onclick="/animes">Animes</li>
+                      <li class="dropdown" onclick="/esportes">Esportes</li>
+                      <li class="dropdown" onclick="/celebridades">Celebridades</li>
+                      <li class="dropdown" onclick="/herois">Herois</li>
+                      <li class="dropdown" onclick="/games">Games</li>
+                      <li class="dropdown" onclick="/marcas">Marcas famosas</li>
 
-      }}
-    >
-      <DropdownToggle
-        caret
-        nav
-      >
-        Categorias
-      </DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem header>
-          Sobre
-        </DropdownItem>
-        <DropdownItem disabled>
-          Contato
-        </DropdownItem>
-        <DropdownItem>
-          Another Action
-        </DropdownItem>
-        <DropdownItem divider />
-        <DropdownItem>
-          Another Action
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-    <NavItem>
-      <NavLink href="/sobre">
-       Sobre
-      </NavLink>
-    </NavItem>
-    <NavItem>
-      <NavLink href="/contato">
-       Contato
-      </NavLink>
-    </NavItem>
-    <NavItem>
-      <NavLink
-        disabled
-        href="#"
-      >
-        
-      </NavLink>
-    </NavItem>
-  </Nav>
-</div>
+                    </ul>
+                    </li>
+                    <li class="li"><a href="/sobre">Sobre</a></li>
+                    <li class="li"><a href="/contato">Contato</a></li>
+                    <li class="li"><a href="https://www.tiktok.com/@serrablocks"><img width="30px" src="../Assets/SerraBlocks/tiktokColor.png" alt=""/></a></li>
+                    <li class="li"><a href="https://www.instagram.com/serrablocks?r=nametag"><img width="25px" src="../Assets/SerraBlocks/instagramColor.png" alt=""/></a></li>
+                    <li class="li"><a href="https://www.facebook.com/fabricadetijolosrj/"><img width="25px" src="../Assets/SerraBlocks/facebook.png" alt=""/></a></li>
+                    <li class="li"><a href="https://www.youtube.com/watch?v=ZRIW6PdEEh8"><img width="25px" src="../Assets/SerraBlocks/youtube.png" alt=""/></a></li>
+                </ul>
+              </nav>
+            </div>
+
+
+
        
         </>
     );
--- src/styles/global.js
+++ src/styles/global.js
@@ -41,4 +41,87 @@ background-color: #21316B;
 
 }
 
+.menu ul{
+        list-style: none;
+        position: relative;
+        background-color: #3D3D94;
+        }
+
+        .menu ul li{
+          width: 150px;
+          float: left;
+          padding: 5px;
+
+          }
+        
+        .menu ul ul li{
+          margin-left: 5px;
+          font-weight: bold;
+        
+            }
+
+
+          .menu{
+            width: 100%;
+            max-height: 70px;
+            background-color: #3D3D94;
+            font-family: Arial, Helvetica, sans-serif;
+            margin-top: 5px;
+            display: flex;
+            flex-shrink: 9;
+            align-items: center;
+  
+          
+          }
+          .menu a{
+            font-size: 15px;
+            padding: 0px;
+            margin: 0px;
+            text-decoration: none;
+            
+            background-color: #3D3D94;
+            color: white;
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            text-shadow: 2px 2px 2px black;
+            }
+      
+     
+        
+        .menu ul ul{
+        
+        position: absolute;
+        visibility: hidden;
+        }
+        .menu ul li:hover ul{ 
+        visibility: visible;
+        
+        }
+        .menu ul ul li{
+          float: none;
+         
+        }
+        
+        .menu a:hover {
+        color: #FA7E0D;
+        }
+        /*  modificacao do dropdown*/
+        .dropdown{
+          color: white;
+          background-color:  #21316B;
+
+        }
+        .dropdown:hover{
+          color: #FA7E0D;
+          cursor: pointer;
+        }
+        
+        nav a {
+          text-transform: uppercase;
+          color: #000000;
+          font-weight: bold;
+          font-size: 22px;
+          text-decoration: none;
+        }
+
+
 `
