06fe113a81ac23a5116a23c73621a31f55179eec
jorge <jrmaiworm@hotmail.com>
2021-11-29 22:17:45 -0300

    up

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ function App() {
   return (
     <> 
       <LogoHeader />
-      
-      
       <Body />
       <Footer />
       <GlobalStyle />
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-
+import "../../styles/global"
 function Sobre() {
     return (
         <>
-           
+           <h1 id="h1p"></h1>
         </>
     )
 }
